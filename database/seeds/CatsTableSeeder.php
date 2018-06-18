1fb1e8f100b8204d9393a996e9bb3c8616d09d71
xuanquynh <xuanquynhdn95@gmail.com>
2018-06-18 19:41:22 +0700

    create migration and seed

---
+++ database/seeds/CatsTableSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CatsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $currentTime=date('Y-m-d H:i:s');
+    	 DB::table('cats')->insert([
+    	 	['id'=>1,
+    	 	'name'=>'Meo Ba Tu long dai',
+    	 	'date_of_birth'=>date('Y-m-d'),
+    	 	'breed_id'=>1,
+    	 	'created_at'=>$currentTime,
+    	 	'updated_at'=>$currentTime
+    	 	],
+    	 	['id'=>2,
+    	 	'name'=>'Meo Rung nepal',
+    	 	'date_of_birth'=>date('Y-m-d'),
+    	 	'breed_id'=>2,
+    	 	'created_at'=>$currentTime,
+    	 	'updated_at'=>$currentTime
+    	 	],
+    	 	['id'=>3,
+    	 	'name'=>'Meo My long dai',
+    	 	'date_of_birth'=>date('Y-m-d'),
+    	 	'breed_id'=>3,
+    	 	'created_at'=>$currentTime,
+    	 	'updated_at'=>$currentTime
+    	 	]
+    	 ]
+    
+        	
+        );
+    }
+
+}
